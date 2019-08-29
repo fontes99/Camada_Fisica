045220cb4a01a9f3ddf2f058daeec70c33daf9aa
Pedro Fontes <pvfontes1000@gmailcom>
2019-08-29 14:39:39 -0300

    fix

--- P3/run.py
+++ P3/run.py
@@ -83,13 +83,13 @@ if typ == "0":
         
 
         #Recebe Resposta  
-        rxHead = com.rx.getNData(12)
+        rxHead = com.rx.getNData(16)
 
         rxLen = int.from_bytes(rxHead[0:4], byteorder='little')
 
-        compTamanho = rxHead[-2]
+        compTamanho = rxHead[-6]
 
-        erroEoP = rxHead[-1]
+        erroEoP = rxHead[-5]
 
         print("*********************************************")
         print('            PACOTE {} DE {}                '.format(a+1, qPck))
@@ -153,141 +153,60 @@ elif typ == "1":
     A=bytes(bytearray())
     A = bytes("CRRT", 'utf-8')
     print ("Aguardando informações...")
+    print (A)
     server = Server()
 
-    ####################
-    #  Loop de espera  #
-    ####################
     while(com.tx.getIsBussy()):
         pass
 
-    
-    QPackTotal = com.rx.getNData(2)
-    QPackTotalINT = int.from_bytes(QPackTotal, byteorder='little')
-    QPackAtual = com.rx.getNData(2)
-    QPackAtualINT = int.from_bytes(QPackAtual, byteorder='little')
     tamanho = com.rx.getNData(4)
     tip = com.rx.getNData(1)
     stuffedQuant = com.rx.getNData(1)
     resposta_tamanho = com.rx.getNData(1)
     resposta_EOP = com.rx.getNData(1)
-    print("START")
     EOP = bytes({0xF0}) + bytes({0xF1}) + bytes({0xF2}) + bytes({0xF3})
     stuffed = bytes({0x00}) + bytes({0xF0}) + bytes({0x00}) + bytes({0xF1}) + bytes({0x00}) + bytes({0xF2}) + bytes({0x00}) + bytes({0xF3})
 
     print("-------------------------")
-    print("PEGUEI AS INFORMAÇÕES DO HEAD")
+    print("PEGUEI AS INFORMAÇÕES")
 
-    ##################################### 
-    #  Tamanho do arquivo + StuffedLen  #
-    #####################################
-    # rxLen = int.from_bytes(tamanho,byteorder='little') + int.from_bytes(stuffedQuant,byteorder='little')
+    #Tamanho do arquivo + StuffedLen
+    rxLen = int.from_bytes(tamanho,byteorder='little') + int.from_bytes(stuffedQuant,byteorder='little')
 
-    while QPackAtualINT<QPackTotalINT:
-        
-
-        ########################################################
-        # Pegando o Buffer e construindo as variaveis do Head  #
-        ########################################################
-        if QPackAtualINT>0:
-            QPackTotal = com.rx.getNData(2)
-            QPackTotalINT = int.from_bytes(QPackTotal, byteorder='little')
-            QPackAtual = com.rx.getNData(2)
-            QPackAtualINT = int.from_bytes(QPackAtual, byteorder='little')
-            tamanho = com.rx.getNData(4)
-            tip = com.rx.getNData(1)
-            stuffedQuant = com.rx.getNData(1)
-            resposta_tamanho = com.rx.getNData(1)
-            resposta_EOP = com.rx.getNData(1)
-
-        print("Quant Total: ", int.from_bytes(QPackTotal,byteorder="little"))
-        print("Quant Atual: ", int.from_bytes(QPackAtual,byteorder="little"))
-    
-        ##############################################
-        #  Pega as informações conhecendo o tamanho! #
-        ##############################################
-        print(QPackAtualINT,"....",QPackTotalINT)
-
-        if QPackAtualINT==QPackTotalINT-1:
-            tamanhoPack = int.from_bytes(tamanho,byteorder="little")%128
-            rxBuffer, nRx = com.getData(tamanhoPack+len(EOP))
-        else:
-            tamanhoPack = 128
-            rxBuffer, nRx = com.getData(tamanhoPack+len(EOP))
-        
-        #####################################
-        #  Quantidade que realmente chegou  #
-        #####################################
-        tamanhoRecebido = len(rxBuffer)-4
-        envio = tamanhoRecebido.to_bytes(4, byteorder='little')
-
-        ##################
-        #  Verifica EOP  #
-        ##################
-        resposta_EOP = server.achaEOP(rxBuffer,EOP)
-
-        #############################
-        #  Verifica Tamanho Imagem  #
-        #############################
-        if tamanhoRecebido!=tamanhoPack:
-            resposta_tamanho = bytes({0x00})
-        else:
-            resposta_tamanho = bytes({0x01})
-        print()
-        head = QPackTotal+QPackAtual+envio+tip+stuffedQuant+resposta_tamanho+resposta_EOP
-
-        ##################
-        #    RESPOSTA    #
-        ##################
-        com.sendData(head)
-
-        print(resposta_tamanho, "+", resposta_EOP)
-        if resposta_tamanho == bytes({0x01}) and resposta_EOP == bytes({0x02}):
-            QPackAtualINT += 1
-
+    #Pega as informações conhecendo o tamanho!  rxBuffer = rxBuffer[0: s01:] + rxBuffer[s01 + 1::]   
+    rxBuffer, nRx = com.getData(rxLen+len(EOP))
 
     print("-------------------------")
     print("CHECKPOINT")
 
-    image, imageLen = server.organizeFile(EOP,stuffed)
+    image, imageLen, resposta_EOP = server.organizeFile(rxBuffer,EOP,stuffed)
 
     print("-------------------------")
     print("ORGANIZEI")
 
     tipo = server.fileType(tip)
-    print(tipo)
 
-    #####################
-    #  Salva o Arquivo  #
-    #####################
+    #Salva o Arquivo
     with open("SaveImage"+"."+tipo,'wb+') as saved:
         saved.write(image)
 
     print("-------------------------")
     print("SALVEI")
 
-    # #####################################
-    # #  Quantidade que realmente chegou  #
-    # #####################################
-    # envio = imageLen.to_bytes(4, byteorder='little')
+    #Quantidade que realmente chegou
+    envio = imageLen.to_bytes(4, byteorder='little')
 
-    # #############################
-    # #  Verifica Tamanho Imagem  #
-    # #############################
-    # if imageLen!=rxLen:
-    #     resposta_tamanho = bytes({0x00})
-    # else:
-    #     resposta_tamanho = bytes({0x01})
-    # head = QPackTotal+QPackAtual+envio+tip+stuffedQuant+resposta_tamanho+resposta_EOP
+    #Verifica Tamanho Imagem
+    if imageLen!=rxLen:
+        resposta_tamanho = bytes({0x00})
+    else:
+        resposta_tamanho = bytes({0x01})
+    head = envio+tip+stuffedQuant+resposta_tamanho+resposta_EOP
 
-    # ##################
-    # #  Retorna Head  #
-    # ##################
-    # com.sendData(head)
+    #Retorna Head
+    com.sendData(head)
 
-    #########################
-    #  Encerra comunicação  #
-    #########################
+    # Encerra comunicação
     print("-------------------------")
     print("Comunicação encerrada")
     print("-------------------------")
