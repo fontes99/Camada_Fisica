a2c5913f7edf0af37489190797755d87620170e7
Pedro Fontes <pvfontes1000@gmailcom>
2019-10-30 10:49:48 -0300

    P8 finalizado

--- P8/suaBibSignal.py
+++ P8/suaBibSignal.py
@@ -2,6 +2,7 @@
 import numpy as np
 import sounddevice as sd
 import matplotlib.pyplot as plt
+from scipy.signal import butter, lfilter, freqz
 from scipy.fftpack import fft
 from scipy import signal as window
 import suaBibSignal as sig
@@ -34,70 +35,13 @@ class signalMeu:
         plt.plot(x, np.abs(y))
         plt.title('Fourier')
     
-    def makeTone(self, input):
+    def butter_lowpass(self, cutoff, fs, order=5):
+        nyq = 0.5 * fs
+        normal_cutoff = cutoff / nyq
+        b, a = butter(order, normal_cutoff, btype='low', analog=False)
+        return b, a
 
-        freqDeAmostragem = 44100
-
-        duration = 1
-
-        gainX  = 0.2
-        gainY  = 0.2
-
-        s1209 = self.generateSin(1209, gainY, duration,freqDeAmostragem)
-        s1336 = self.generateSin(1336, gainY, duration,freqDeAmostragem)
-        s1477 = self.generateSin(1477, gainY, duration,freqDeAmostragem)
-        s1633 = self.generateSin(1633, gainY, duration,freqDeAmostragem)
-        s697 =  self.generateSin(697,  gainY, duration,freqDeAmostragem)
-        s770 =  self.generateSin(770,  gainY, duration,freqDeAmostragem)
-        s852 =  self.generateSin(852,  gainY, duration,freqDeAmostragem)
-        s941 =  self.generateSin(941,  gainY, duration,freqDeAmostragem)
-
-        if input == '1':
-            return s1209[1]+s697[1],s1209,s697
-
-        elif input == '2':
-            return s1336[1]+s697[1],s1336,s697
-
-        elif input == '3':
-            return s1477[1]+s697[1],s1477,s697
-
-        elif input == '4':
-            return s1209[1]+s770[1],s1209,s770
-
-        elif input == '5':
-            return s1336[1]+s770[1],s1336,s770
-
-        elif input == '6':
-            return s1477[1]+s770[1],s1477,s770
-
-        elif input == '7':
-            return s1209[1]+s852[1],s1209,s852
-
-        elif input == '8':
-            return s1336[1]+s852[1],s1336,s852
-
-        elif input == '9':
-            return s1477[1]+s852[1],s1477,s852
-
-        elif input == '0':
-            return s1336[1]+s941[1],s1336,s941
-
-        elif input == 'X':
-            return s1209[1]+s941[1],s1209,s941
-
-        elif input == '#':
-            return s1477[1]+s941[1],s1477,s941
-
-        elif input == 'A':
-            return s1633[1]+s697[1],s1633,s697
-
-        elif input == 'B':
-            return s1633[1]+s770[1],s1633,s770
-
-        elif input == 'C':
-            return s1633[1]+s852[1],s1633,s852
-
-        elif input == 'D':
-            return s1633[1]+s941[1],s1633,s941
-
-
+    def butter_lowpass_filter(self, data, cutoff, fs, order=5):
+        b, a = self.butter_lowpass(cutoff, fs, order=order)
+        y = lfilter(b, a, data)
+        return y
